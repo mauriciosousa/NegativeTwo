dbdf4584c7aa0e68116aa7818262d136bbc81402
Mauricio Sousa <antonio.sousa@ist.utl.pt>
2018-05-17 16:26:10 +0100

    Agora tem um mars rover :wheelchair:

---
+++ NegativeTwo/Assets/Scripts/PlaceMockupObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceMockupObject : MonoBehaviour {
+
+    public Transform target;
+    private Transform origin;
+
+	// Use this for initialization
+	void Start ()
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (origin != null)
+        {
+            target.position = origin.position;
+            target.rotation = origin.rotation;
+        }
+        else
+        {
+            origin = GameObject.Find("workspaceCollider").transform;
+        }
+	}
+}
