3171f4be13657675c6efdbaef0ef9ec041462bc4
Mauricio Sousa <antonio.sousa@ist.utl.pt>
2018-06-27 12:04:15 +0100

    pole :pouch:

---
+++ PointingEvaluation/Assets/Scripts/Pole.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Pole : MonoBehaviour {
+
+    public GameObject poleCellPrefab;
+
+    public float poleCellSize; // (4cm)
+    public float h0; // bottom (32cm)
+    public float h1; // top (284 cm)
+
+    public int numberOfPoleCells;
+
+    private List<GameObject> poleCells;
+
+	void Start ()
+    {
+        poleCells = new List<GameObject>();
+
+        numberOfPoleCells = (int) ((h1 - h0) / poleCellSize);
+        for (int i = 0; i < numberOfPoleCells; i++)
+        {
+            GameObject cell = Instantiate(poleCellPrefab, transform, false);
+            
+            cell.transform.localScale = new Vector3(poleCellSize, poleCellSize, poleCellSize);
+            cell.transform.position += cell.transform.up * poleCellSize * (i + 1);
+            cell.GetComponent<PoleNumber>().textMesh.text = "" + i + 1;
+        }
+
+        print("CELLLLSSSSS  " + numberOfPoleCells);
+	}
+	
+	void Update () {
+		
+	}
+}
---
+++ PointingEvaluation/Assets/Scripts/PoleNumber.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoleNumber : MonoBehaviour {
+
+    public Transform quad;
+    public Transform text;
+    public TextMesh textMesh;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
